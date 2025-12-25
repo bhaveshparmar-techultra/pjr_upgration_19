3414382d511bb9f238b12f1369580a0dd28a0916
bhaveshtechultra <bhaveshparmar.techultra@gmail.com>
2025-12-25 11:12:02 +0530

    [MOD]add sale_Crm_dynamic_Approval module

---
+++ sale_crm_dynamic_approval/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Sales CRM Dynamic Approval',
+    'summary': 'Integration sales with crm',
+    'author': 'Ever Business Solutions',
+    'maintainer': 'Abdalla Mohamed',
+    'website': 'https://www.everbsgroup.com/',
+    'version': '19.0.1.0.0',
+    'category': 'Accounting/Accounting',
+    'license': 'OPL-1',
+    'depends': [
+        'sale_dynamic_approval',
+        'sale_crm',
+    ],
+    'data': [
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
